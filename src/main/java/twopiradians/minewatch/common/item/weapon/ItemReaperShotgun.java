f09df8ed2bb87970cce16df85f50116e19226ab1
Furgl <jake.pollard15@gmail.com>
2017-08-29 23:23:22 -0500

    cleaned up tp code - still need to do particle magic

--- src/main/java/twopiradians/minewatch/common/item/weapon/ItemReaperShotgun.java
+++ src/main/java/twopiradians/minewatch/common/item/weapon/ItemReaperShotgun.java
@@ -1,16 +1,16 @@
 package twopiradians.minewatch.common.item.weapon;
 
+import javax.annotation.Nullable;
+
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.EnumHand;
 import net.minecraft.util.EnumParticleTypes;
 import net.minecraft.util.SoundCategory;
-import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.RayTraceResult;
 import net.minecraft.util.math.Vec3d;
 import net.minecraft.world.World;
-import net.minecraft.world.WorldServer;
 import net.minecraftforge.fml.common.network.NetworkRegistry.TargetPoint;
 import twopiradians.minewatch.common.Minewatch;
 import twopiradians.minewatch.common.entity.EntityMWThrowable;
@@ -20,7 +20,7 @@ import twopiradians.minewatch.common.sound.ModSoundEvents;
 import twopiradians.minewatch.packet.SPacketSpawnParticle;
 
 public class ItemReaperShotgun extends ItemMWWeapon {
-	
+
 	public ItemReaperShotgun() {
 		super(30);
 		this.hasOffhand = true;
@@ -49,37 +49,41 @@ public class ItemReaperShotgun extends ItemMWWeapon {
 		}
 	}
 
+	@Nullable
+	private Vec3d getTeleportPos(EntityPlayer player) {
+		RayTraceResult result = player.world.rayTraceBlocks(player.getPositionEyes(1), 
+				player.getLookVec().scale(Integer.MAX_VALUE), false, false, true);
+		if (result != null && result.typeOfHit == RayTraceResult.Type.BLOCK && result.hitVec != null && 
+				player.world.isAirBlock(result.getBlockPos().up()) && player.world.isAirBlock(result.getBlockPos().up(2)) && 
+				Math.sqrt(result.getBlockPos().distanceSq(player.posX, player.posY, player.posZ)) <= 35)
+			return new Vec3d(result.hitVec.xCoord, result.getBlockPos().getY()+1, result.hitVec.zCoord);
+		return null;
+	}
+
 	@Override
 	public void onUpdate(ItemStack stack, World world, Entity entity, int itemSlot, boolean isSelected) {
 		super.onUpdate(stack, world, entity, itemSlot, isSelected);
 
+		// teleport
 		if (entity instanceof EntityPlayer) {
-
 			EntityPlayer player = (EntityPlayer)entity;
-
-			if (!world.isRemote && hero.ability1.isSelected(player)) {   
-
-				Vec3d vecPos = player.getPositionEyes(1);
-				Vec3d vec = player.getLookVec().scale(35);
-				Vec3d vecLook = vec.addVector(vec.xCoord * 40, vec.yCoord * 40, vec.zCoord * 40);
-												
-				if (world.rayTraceBlocks(vecPos, vecLook, false, false, true) != null && world.rayTraceBlocks(vecPos, vecLook, false, false, true).typeOfHit == RayTraceResult.Type.BLOCK) {
-					
-					BlockPos pos = world.rayTraceBlocks(vecPos, vecLook, false, false, true).getBlockPos();
-					
-					((WorldServer)world).spawnParticle(EnumParticleTypes.HEART, pos.getX() + 0.5d, pos.getY()+world.rand.nextDouble()*2, pos.getZ() + 0.5d, 10, 0, 0, 0, 0, new int[0]);
-					
-					if (Minewatch.keys.rmb(player) && player.attemptTeleport(pos.getX() + 0.5d, pos.getY()+1, pos.getZ() + 0.5d)) {//if pos found and can tp
-						if (player.isRiding())
-							player.dismountRidingEntity();
-						/*world.playSound((EntityPlayer)null, player.posX, player.posY, player.posZ, SoundEvents.ITEM_CHORUS_FRUIT_TELEPORT, SoundCategory.PLAYERS, 1.0F, 1.0F);
-					world.playSound((EntityPlayer)null, pos.getX(), pos.getY(), pos.getZ(), SoundEvents.ITEM_CHORUS_FRUIT_TELEPORT, SoundCategory.PLAYERS, 1.0F, 1.0F);
-					player.playSound(SoundEvents.ITEM_CHORUS_FRUIT_TELEPORT, 1.0F, 1.0F);
-					for (int j = 0; j < 64; ++j) {
-						((WorldServer)world).spawnParticle(EnumParticleTypes.PORTAL, player.posX+2*world.rand.nextDouble(), player.posY+world.rand.nextDouble()+1, player.posZ+2*world.rand.nextDouble(), 1, 0, 0, 0, 1, new int[0]);
-						((WorldServer)world).spawnParticle(EnumParticleTypes.PORTAL, player.posX+2*world.rand.nextDouble(), player.posY+world.rand.nextDouble()+1.0D, player.posZ+2*world.rand.nextDouble(), 1, 0, 0, 0, 1, new int[0]);
-					}*/
-						hero.ability1.keybind.setCooldown(player, 20, false); 
+			if (hero.ability1.isSelected(player) && this.canUse(player, true)) {   
+				if (world.isRemote) {
+					Vec3d tpVec = this.getTeleportPos(player);
+					if (tpVec != null) 
+						for (int i=0; i<5; ++i)
+							world.spawnParticle(EnumParticleTypes.HEART, tpVec.xCoord, tpVec.yCoord+world.rand.nextDouble()*2, tpVec.zCoord, 0, 0, 0);
+				}
+				else if (Minewatch.keys.rmb(player)) {
+					Vec3d tpVec = this.getTeleportPos(player);
+					if (tpVec != null) {
+						if (player.attemptTeleport(tpVec.xCoord, tpVec.yCoord, tpVec.zCoord)) {
+							if (player.isRiding())
+								player.dismountRidingEntity();
+							hero.ability1.keybind.setCooldown(player, 20, false); 
+						}
+						else // otherwise holding rmb on place you can't tp will freeze player in air
+							player.getCooldownTracker().setCooldown(this, 20);
 					}
 				}
 			}

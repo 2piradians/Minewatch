5a28752bbe91d54b5514b3b9ad88f21b6840389d
2piradians <2piradians314@gmail.com>
2017-09-03 09:35:55 -0500

    Fixed entities not having motion on client.

--- src/main/java/twopiradians/minewatch/common/Minewatch.java
+++ src/main/java/twopiradians/minewatch/common/Minewatch.java
@@ -21,7 +21,7 @@ public class Minewatch
 {
     public static final String MODNAME = "Minewatch";
     public static final String MODID = "minewatch";
-    public static final String VERSION = "3.2";
+    public static final String VERSION = "3.2.2";
     @Mod.Instance(MODID)
     public static Minewatch instance;
     public static MinewatchTab tab = new MinewatchTab("tabMinewatch");
